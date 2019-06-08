c9e782d7bdfcc759b97d15350766d729c8cc477f
suhanlee <suhan.lee.k@gmail.com>
2019-06-07 20:34:00 -0700

    add flat function

---
+++ src/flat.js
@@ -0,0 +1,11 @@
+const flat = (args) => {
+    const _flat = (args) => {
+        const [first, tail] = [args.shift(), args]
+        if (first === undefined) return [];
+        return typeof first === 'object' && first.length > 0 ? first.concat(_flat(tail)) : [first].concat(_flat(tail));
+    }
+
+    return _flat(args);
+}
+
+module.exports = flat;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const sum = require('./sum');
 const map = require('./map');
 const reduce = require('./reduce');
 const pipe = require('./pipe');
+const flat = require('./flat');
+
 module.exports = {
     sum,
     map,
     reduce,
     pipe,
+    flat,
 }
