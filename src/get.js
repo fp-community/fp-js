6b3ef9ef64d5e5de2642997f26e8d4ab9fbc564a
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-06-13 17:20:50 +0900

    Remove eval from get

--- src/get.js
+++ src/get.js
@@ -1,6 +1,15 @@
 const get = attr => obj => {
-  const value = eval(`obj.${attr}`)
-  return value ? value : null
+  if (!obj) return
+
+  if (!typeof attr === 'string') return
+
+  if (attr.match(/\./)) {
+    const [first, ...rest] = attr.split('.')
+
+    return get(rest.join(''))(obj[first])
+  }
+
+  return obj[attr]
 }
 
 module.exports = get
