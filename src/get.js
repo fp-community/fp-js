ffe6d0b18c94e6ecbcba600d002d486bf98c5859
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-06-13 18:13:40 +0900

    Fix string type checking condition

--- src/get.js
+++ src/get.js
@@ -1,15 +1,15 @@
-const get = attr => obj => {
-  if (!obj) return
+const get = (attr) => (obj) => {
+  if (!obj) return;
 
-  if (!typeof attr === 'string') return
+  if (typeof attr !== 'string') return;
 
   if (attr.match(/\./)) {
-    const [first, ...rest] = attr.split('.')
+    const [first, ...rest] = attr.split('.');
 
-    return get(rest.join(''))(obj[first])
+    return get(rest.join(''))(obj[first]);
   }
 
-  return obj[attr]
-}
+  return obj[attr];
+};
 
-module.exports = get
+module.exports = get;
